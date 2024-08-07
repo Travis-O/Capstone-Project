be375fdba4915290695615096d2911d1c8197fe1
Travis-O <149115697+Travis-O@users.noreply.github.com>
2024-08-07 12:40:18 -0400

    Commit #7
    I fixed my google maps API to follow guidelines, now completing all the features my website needs, and now I will focus on finishing the website and adding style. (I still kept the original maps in other pages, but the correct API is in the nextstep.html page.)

---
+++ Capstone Project/map.js
@@ -0,0 +1,16 @@
+let map;
+
+async function initMap() {
+ 
+    const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 38.79390, lng: -84.56015 },
+    zoom: 13,
+  });
+}
+
+initMap();
+
+
+
